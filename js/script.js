2695690684029becfe66b4ae75ab411f7cce9cc3
Bjorkbat <bjorkbat@gmail.com>
2016-11-02 11:28:32 -0600

    Added an API key

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY";
+var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=LJJGFjN32CHcHkl6CNyssAmzm1OLYRSJ4dcMBCdQ";
 
 $.ajax({
   url: url,
